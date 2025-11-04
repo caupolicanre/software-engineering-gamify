6ca5df99a9306bc3e53d534e8df69427efda3daa
caupolicanre <caupolicanre@gmail.com>
2025-11-04 02:54:20 -0300

    feat: Add initial API structure for achievements with URL routing and views

--- src/apps/achievements/api/urls.py
+++ src/apps/achievements/api/urls.py
@@ -1,17 +1,15 @@
-"""
-URL configuration for Achievement API.
-"""
+"""URL configuration for Achievement API."""
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.achievements.api.viewsets import AchievementViewSet
+from . import views
 
 
 app_name = "achievements"
 
 router = DefaultRouter()
-router.register(r"achievements", AchievementViewSet, basename="achievement")
+router.register(r"achievements", views.AchievementViewSet, basename="achievement")
 
 urlpatterns = [
     path("", include(router.urls)),
